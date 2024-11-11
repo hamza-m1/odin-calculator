cbc0acc8ef8b8f3f6fb7a219f67598f3f0bf79d1
Hamza <177459013+hamza-m1@users.noreply.github.com>
2024-11-11 13:23:46 +0000

    Add operate function

--- script.js
+++ script.js
@@ -2,17 +2,46 @@
 
 
 function add(num1, num2) {
-    return num1 + num2
+  return num1 + num2
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+  return num1 - num2
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+  return num1 * num2
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+  return num1 / num2
 }
+
+let number1;
+let number2;
+let operator;
+
+function operate(number1, number2, operator) {
+  result = 0
+  switch (operator) {
+    case '+':
+      result = add(number1, number2)
+      break;
+    case '-':
+      result = subtract(number1, number2)
+      break;
+    case '*':
+      result = multiply(number1, number2)
+      break;
+    case '/':
+      result = divide(number1, number2)
+      break
+  }
+  return result
+}
+
+// number1 = 2
+// number2 = 4
+// operator = '/'
+
+// console.log(operate(number1, number2, operator))
